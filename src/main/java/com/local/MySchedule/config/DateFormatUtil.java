7a564eea4a2ca8134af163b7f3d89a5e4f8e5c00
QQDDTT <hunengwei0414@gmail.com>
2024-11-02 20:19:36 +0900

    20241102

--- src/main/java/com/local/MySchedule/config/DateFormatUtil.java
+++ src/main/java/com/local/MySchedule/config/DateFormatUtil.java
@@ -2,6 +2,8 @@ package com.local.MySchedule.config;
 
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -11,6 +13,7 @@ import java.time.LocalTime;
 public class DateFormatUtil {
     private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     private final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+    private static final double WIDTH_NUM = 0.78;
 
     // 格式化 LocalTime
     public String formatTime(LocalTime time) {
@@ -20,5 +23,15 @@ public class DateFormatUtil {
     // 格式化 LocalDate
     public String formatDate(LocalDate date) {
         return date != null ? date.format(dateFormatter) : LocalDate.now().format(dateFormatter);
+    }
+
+    public int calculateLeftPosition(LocalTime startTime) {
+        int left =  startTime.getHour() * 60 + startTime.getMinute(); // 每小时60px
+        return (int) (left * WIDTH_NUM);
+    }
+    
+    public int calculateWidth(LocalTime startTime, LocalTime endTime) {
+        long durationInMinutes = Duration.between(startTime, endTime).toMinutes();
+        return (int) (durationInMinutes * WIDTH_NUM); // 每分钟1px
     }
 }
--- src/main/java/com/local/MySchedule/controller/ListController.java
+++ src/main/java/com/local/MySchedule/controller/ListController.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.LinkedHashMap;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
